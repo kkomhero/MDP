ebb4a054d6d762b8b3e03fdb3caa6ea0b5396ad3
Lamhirh <lamhirh@Lamhirh-MacBook15.local>
2019-09-06 11:16:43 +0900

    spring cloud test

--- testApp/gateway-test/discovery-service/src/main/java/com/mbg/otdev/discoveryservice/MdpEventHandler.java
+++ testApp/gateway-test/discovery-service/src/main/java/com/mbg/otdev/discoveryservice/MdpEventHandler.java
@@ -1,41 +1,44 @@
 package com.mbg.otdev.discoveryservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class MdpEventHandler {
+    private static final Logger log = LoggerFactory.getLogger(MdpEventHandler.class);
+
+    @Value("${uris.baseUrl}")
+    private String base_url;
+
+    @Value("${uris.jobcontrollerUrl.baseUrl}")
+    private String jobcontroller_url;
+
+    @Value("${uris.jobcontrollerUrl.serviceStatus}")
+    private String jobcontroller_starus_url;
 
     @EventListener
     public void handle(EurekaInstanceCanceledEvent cancleEvent) {
-        System.out.println("*** MdpEventHandler=>"+cancleEvent.getAppName()+">>"+cancleEvent.getServerId());
+        
+        log.info("*** MdpEventHandler=>"+cancleEvent.getAppName()+">>"+cancleEvent.getServerId());
 
-        RestTemplate restTemplate = new RestTemplate();
- 
-        HttpEntity<Foo> request = new HttpEntity<>(new Foo("bar"));
-        Foo foo = restTemplate.postForObject(fooResourceUrl, request, Foo.class);
+        //String request_url = base_url+"/"+jobcontroller_url+"/"+jobcontroller_starus_url+"/"+cancleEvent.getAppName()+"/"+cancleEvent.getServerId();
+        String request_url = jobcontroller_starus_url+"/"+cancleEvent.getAppName()+"/"+cancleEvent.getServerId();
+        log.info("request url = "+request_url);
 
-/*
-        RestTemplate restTemplate = new RestTemplate();
-
-    String reqBody = "{"city": "Ranchi"}";
-    String result = restTemplate.postForObject(uri, reqBody, String.class);
-
-    // convert your result into json
-
-    try {
-                jsonResponse = new JSONObject(result);
-        } catch (JSONException e) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<Void> responseEntity=restTemplate.exchange(request_url, HttpMethod.PUT,null,Void.class);
+            log.info("status code="+responseEntity.getStatusCode());
+        }
+        catch(Exception e) {
             e.printStackTrace();
         }
-   //extract a value "name" from your json data:
-   try{
-    String value = jsonResponse.getString("name");  
-    }catch(JSONException e) {
-            e.printStackTrace();
-        }
-
-        */
     }
 }
